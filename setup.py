9aa49f3f01b877c22cb272b9086a86ea15e58cd4
Dmitriy Ferens <ferensdima@gmail.com>
2020-03-22 20:43:25 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from pathlib import Path
 
-version = (0, 0, 1)
+version = (0, 1, 0)
 package_name = next(Path(__file__).parent.joinpath('src').iterdir()).name
 
 with open('requirements.txt') as fp:
